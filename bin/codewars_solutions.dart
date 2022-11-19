c35446cfee0348c8dbc79326beb967d8f44190e0
Kves <patryk@MacBook-Pro-Patryk.local>
2022-11-19 15:49:17 +0100

    initial commit

---
+++ bin/codewars_solutions.dart
@@ -0,0 +1,5 @@
+import 'package:codewars_solutions/codewars_solutions.dart' as codewars_solutions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codewars_solutions.calculate()}!');
+}
---
+++ test/codewars_solutions_test.dart
@@ -0,0 +1,8 @@
+import 'package:codewars_solutions/codewars_solutions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
