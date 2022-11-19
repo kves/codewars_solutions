881c55678f63655e084ef606a6108aa6d64d9465
Kves <patryk@MacBook-Pro-Patryk.local>
2022-11-19 16:24:47 +0100

    grasshopper - summation

--- bin/codewars_solutions.dart
+++ bin/codewars_solutions.dart
@@ -1,7 +1,8 @@
 import 'package:codewars_solutions/count_by_x.dart';
 import 'package:codewars_solutions/grasshopper_grade_book.dart';
+import 'package:codewars_solutions/grasshopper_summation.dart';
 import 'package:codewars_solutions/sum_arrays.dart';
 
 void main(List<String> arguments) {
-  print(sum([23, 34, 54, 23]));
+  print(summation(2));
 }
---
+++ lib/grasshopper_summation.dart
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+int summation(int n) {
+  int result = 0;
+  for (var i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
+}
