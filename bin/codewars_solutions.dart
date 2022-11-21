22c8a33c2e49b78960820ec8d4ade4698a39d840
Kves <vnlkbn@gmail.com>
2022-11-21 18:57:47 +0100

    convert string to a number

--- bin/codewars_solutions.dart
+++ bin/codewars_solutions.dart
@@ -1,3 +1,4 @@
+import 'package:codewars_solutions/convert_string_to_a_number.dart';
 import 'package:codewars_solutions/count_by_x.dart';
 import 'package:codewars_solutions/grasshopper_grade_book.dart';
 import 'package:codewars_solutions/grasshopper_summation.dart';
@@ -6,5 +7,5 @@ import 'package:codewars_solutions/reversed_string.dart';
 import 'package:codewars_solutions/sum_arrays.dart';
 
 void main(List<String> arguments) {
-  print(nbYear(1500, 5, 100, 5000));
+  print(stringToNumber('2323'));
 }
---
+++ lib/convert_string_to_a_number.dart
@@ -0,0 +1,4 @@
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
+int stringToNumber(String str) {
+  return int.parse(str);
+}
