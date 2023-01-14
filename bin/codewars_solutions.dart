bb509b62fbbe5fac7f6e5b4d5f50ebab690af706
Kves <vnlkbn@gmail.com>
2023-01-14 23:21:16 +0100

    youre square

--- bin/codewars_solutions.dart
+++ bin/codewars_solutions.dart
@@ -1,5 +1,6 @@
 import 'package:codewars_solutions/minimize_sum_of_array.dart';
+import 'package:codewars_solutions/youre_square.dart';
 
 void main(List<String> arguments) {
-  print(minSum([1, 0, 5, 10]));
+  print(isSquare(24));
 }
---
+++ lib/youre_square.dart
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+import 'dart:math';
+
+isSquare(n) => sqrt(n) == sqrt(n).roundToDouble() ? true : false;
